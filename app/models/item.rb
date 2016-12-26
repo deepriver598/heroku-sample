f67016c1a22ce399f7b714157997109bbdd13441
deepriver598 <biz.deepriver@gmail.com>
2016-12-26 11:40:27 +0900

    itemにvalidationを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ActiveRecord::Base
+  validates :name, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, length: { maximum: 255 }
 end
